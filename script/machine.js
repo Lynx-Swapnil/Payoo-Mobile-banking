7142ec78a2e7cbdd8eec1d14d799c18e37af818a
Lynx-Swapnil <lynxswapnil@gmail.com>
2026-02-19 02:38:00 +0600

    all pages added

--- script/machine.js
+++ script/machine.js
@@ -21,15 +21,41 @@ function setBalance(value) {
 }   
 
 function showOnly(id) {
+    const latestPayment = document.getElementById("latest-payment");
     const addMoney = document.getElementById("add-money");
     const cashout = document.getElementById("cashout");
+    const transferMoney = document.getElementById("transfer-money");
+    const getBonus = document.getElementById("get-bonus");
+    const payBill = document.getElementById("pay-bill");
     const history = document.getElementById("history");
 
+    latestPayment.classList.add("hidden");
     addMoney.classList.add("hidden");
     cashout.classList.add("hidden");
+    transferMoney.classList.add("hidden");
+    getBonus.classList.add("hidden");
+    payBill.classList.add("hidden");
     history.classList.add("hidden");
 
     const selected = document.getElementById(id);
     selected.classList.remove("hidden");
     
+}
+
+function showHome() {
+    const latestPayment = document.getElementById("latest-payment");
+    const addMoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+    const transferMoney = document.getElementById("transfer-money");
+    const getBonus = document.getElementById("get-bonus");
+    const payBill = document.getElementById("pay-bill");
+    const history = document.getElementById("history");
+
+    latestPayment.classList.remove("hidden");
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+    transferMoney.classList.add("hidden");
+    getBonus.classList.add("hidden");
+    payBill.classList.add("hidden");
+    history.classList.add("hidden");
 }
